ca82c07c595727fe7ac91a7166e46da5fd538938
sgio <sergiobenllochlopez@gmail.com>
2023-04-12 17:33:07 +0200

    Fix bug

--- binsniff/assemparser.py
+++ binsniff/assemparser.py
@@ -225,11 +225,9 @@ def _timed_cfg(binary_path, timeout):
     return cfg
 
 def get_timed_cfg(timeout, binary):
-    try:
-        cfg = _timed_cfg(binary, timeout)
-        return cfg
-    except TimeoutError:
-        raise Exception("Timeout in CFG")
+    cfg = _timed_cfg(binary, timeout)
+    return cfg
+
 
 def assemparse(binary, timeout) -> tuple[dict, bool]:
     """
